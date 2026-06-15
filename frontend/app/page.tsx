118f2b93afe042c72a85fc319c61f279d795ff02
xenon <demiladeafuape74@gmail.com>
2026-06-15 19:17:22 +0100

    updated main page to display landing page instead of dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,7 @@
 import Dashboard from "./component/Dashboard";
+import LandingPage from "./landing-page/page";
 
 export default function Home() {
-  return <Dashboard />;
+  return  <LandingPage />;
+  // <Dashboard />;
 }
