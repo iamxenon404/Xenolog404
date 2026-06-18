33423ccce4f20f081195166bd52c70d6f6a5ea29
xenon <demiladeafuape74@gmail.com>
2026-06-18 19:12:19 +0100

    imported dashboard in dashboard page

---
+++ frontend/app/dashboard/Page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import Dashboard from "./component/Dashboard";
+
+// 🍏 Import your existing Dashboard layout component
+// import Dashboard from "../component/Dashboard"; 
+
+export default function DashboardPage() {
+  return <Dashboard />;
+}
